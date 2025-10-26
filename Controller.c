6c9e1d5ab59e5a89721d98f444cecb44eb41cd90
Ernest Blake <59033052+Striker172@users.noreply.github.com>
2025-10-26 15:36:48 -0500

    Methods
    Added some methods for the RFID stuff

--- Controller.c
+++ Controller.c
@@ -5,6 +5,11 @@
 volatile bool hasBalance = false;
 volatile bool sendHIDReport = true;
 MIFARE_Key key = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
+static Uid OwnersUID = {
+    .uidByte = {},
+    .sak = 1,
+    .size =1 
+};
 bool repeating_timer_callback(__unused struct repeating_timer *t){
 
 }
@@ -12,6 +17,26 @@ int64_t alarm_callback(alarm_id_t id, __unused void *user_data){
     //This will be used to disable user input
     hasBalance = false;
     return 0;
+}
+bool makeOwnerCard(MFRC522Ptr_t mfrc522){
+    //bool MIFARE_SetUid, 
+    bool Errors = 0;
+    // while(!PICC_IsNewCardPresent(mfrc522)); Assuming this has already been called before the command
+    PICC_Select(mfrc522,&(mfrc522->uid),0);
+    if(memcmp(&mfrc522->uid,&OwnersUID,sizeof(OwnersUID)) != 0){
+        MIFARE_SetUid(mfrc522,&OwnersUID,OwnersUID.size,Errors);
+    }
+    return Errors;
+}
+StatusCode writeToCard(MFRC522Ptr_t mfrc522,uint8_t sector,char *message){
+    StatusCode code = PCD_Authenticate(mfrc522,PICC_CMD_MF_AUTH_KEY_A,sector,&key,&(mfrc522->uid));
+    if(code == 0){
+        code = MIFARE_Write(mfrc522,0x04,message,sizeof(message));
+        if(code == 0){
+            PCD_StopCrypto1(mfrc522);
+        } 
+    }
+    return code;
 }
 int main()
 {
@@ -29,9 +54,8 @@ int main()
             printf("Card detected!\n\r");
             PICC_ReadCardSerial(mfrc522);
             PICC_DumpToSerial(mfrc522,&(mfrc522->uid)); //This says uid, type of card and such. 
-            PCD_Authenticate(mfrc522,PICC_CMD_MF_AUTH_KEY_A,0x06,&key,&(mfrc522->uid));
-            MIFARE_Write(mfrc522,0x06,message,sizeof(message));
-            PCD_StopCrypto1(mfrc522);
+            //Block address equation = (Sector Num *4) +Block Offset
+            writeToCard(mfrc522,0x04,message);
         }
         /* This should how the setup should go for the timer
         Read the actual balance on the RFID card read the flag whether it should remove the balance or not, 
